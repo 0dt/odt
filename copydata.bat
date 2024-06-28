48e5e6f944258305468332b7e7e251e97931f4ac
Anre Kehlert <44321320+0dt@users.noreply.github.com>
2024-06-29 01:01:54 +0200

    Add files via upload

--- copydata.bat
+++ copydata.bat
@@ -1,6 +1,10 @@
 VERSION: 2022-03-23-V10
 path C:\Windows\System32
-robocopy D:\nkosana F:\nkosana /MIR /FFT /Z /XA:H /W:5 /LOG+:F:\Batch\logs\logs.txt /TEE
+# Backup to another system on the network
+robocopy D:\dir1 \\computer_on_network\dir1 /MIR /FFT /Z /XA:H /W:5 /LOG+:F:\Batch\logs\logs.txt /TEE
+#Back data from Drive D to Drive F 
+robocopy D:\dir1 F:\dir1 /MIR /FFT /Z /XA:H /W:5 /LOG+:F:\Batch\logs\logs.txt /TEE
+# makes a log file of the backup
 Set FileDate=%date:/=%
 ren F:\Batch\logs.txt logs%FileDate%.txt
 del F:\Batch\logs\logs.txt
